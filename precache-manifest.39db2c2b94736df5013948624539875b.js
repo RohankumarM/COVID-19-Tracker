11a804e3c3989d16a5fa642a09278b64a8b46f4c
Rohankumar Maisuria <rmaisuria@horizon.csueastbay.edu>
2020-07-18 15:38:17 +0530

    Updates

---
+++ precache-manifest.39db2c2b94736df5013948624539875b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ca5364f4d3f9eeabed10596fce6513",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "10cad11d1f77df3d255c",
+    "url": "/COVID-19-Tracker/static/css/main.3b5a48ea.chunk.css"
+  },
+  {
+    "revision": "a410fe596342269e677d",
+    "url": "/COVID-19-Tracker/static/js/2.af6cb1c3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker/static/js/2.af6cb1c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10cad11d1f77df3d255c",
+    "url": "/COVID-19-Tracker/static/js/main.ac211fa6.chunk.js"
+  },
+  {
+    "revision": "fe188a24c2a3117838f8",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.5bf882c0.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
